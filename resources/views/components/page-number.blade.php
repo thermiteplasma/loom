08fcf55f3cef7552833cf8dc21c6ac7a3980cc30
David Bell <dbell@agtslots.com>
2026-03-30 09:53:34 +1100

    fix typo

--- resources/views/components/page-number.blade.php
+++ resources/views/components/page-number.blade.php
@@ -1,4 +1,4 @@
 <span style="{{ $styles() }}" {{ $attributes }}>
-    {{- $prefix }}<span class="page-number"></span>
+    {{ $prefix }}<span class="page-number"></span>
     @if($showTotal){{ $separator }}<span class="total-pages"></span>@endif
 </span>
